268e19899d0a60113501f54a5b2add29b7623d17
jieun4510@sju.ac.kr <jieun4510@sju.ac.kr>
2024-03-31 02:32:27 +0900

    feat: 01-01 task 조회

--- task/urls.py
+++ task/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.TaskAPIView.as_view()),
     path('list/', views.TaskListAPIView.as_view()),
     path('list/date/', views.TaskDateListAPIView.as_view()),
+    path('<int:pk>/', views.TaskEachAPIView.as_view()),
 ]
--- task/views.py
+++ task/views.py
@@ -7,7 +7,6 @@ from task.models import Task
 from task.serializers import TaskSerializer
 
 
-# Create your views here.
 class TaskAPIView(APIView):
     # 01-02 task 생성
     def post(self, request):
@@ -16,6 +15,19 @@ class TaskAPIView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class TaskEachAPIView(APIView):
+    # 01-01 task 조회
+    def get(self, request, pk):
+        try:
+            task = Task.objects.get(pk=pk)
+            serializer = TaskSerializer(task)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except Task.DoesNotExist:
+            return Response({'message': '해당 id의 task가 없습니다.'}, status=status.HTTP_400_BAD_REQUEST)
+
+
+
 
 
 class TaskListAPIView(APIView):
