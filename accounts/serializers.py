c63d49f8367bf38a08e58c64d175541f3dc461f5
jieun4510@sju.ac.kr <jieun4510@sju.ac.kr>
2024-04-16 13:51:07 +0900

    Feat: 04 회원관리

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,8 +1,24 @@
+from allauth.account.adapter import get_adapter
+from dj_rest_auth.registration.serializers import RegisterSerializer
+from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
+
 from accounts.models import User
 
 
 class UserSerializer(ModelSerializer):
+    # email = serializers.EmailField(required=True, max_length=255)
+    # password = serializers.CharField(max_length=100, write_only=True)
+    # nickname = serializers.CharField(max_length=50)
+
     class Meta:
         model = User
         fields = '__all__'
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            email=validated_data['email'],
+            nickname=validated_data['nickname'],
+            password=validated_data['password']
+        )
+        return user
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,3 +1,9 @@
-urlpatterns = [
+from dj_rest_auth.registration.views import RegisterView
+from django.urls import include, path
+from accounts.views import SignUpAPIView, AuthAPIView
 
+urlpatterns = [
+    # path('', include('dj_rest_auth.urls')),
+    path('signup/', SignUpAPIView.as_view()),
+    path('auth/', AuthAPIView.as_view()),
 ]
